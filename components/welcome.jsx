bb2bf7fb3dbf701dc2b01b09e740e26bd2390c8f
WoodShack <woodhousie@gmail.com>
2025-09-25 09:08:46 -0400

    Add welcome component

---
+++ components/welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            <h2>Welcome to CPAN114 Assignment 1</h2>
+            <h2>Scott Woodhouse | N01774081</h2>
+        </div>
+    );
+};
+
+export default Welcome;
