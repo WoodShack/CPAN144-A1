73223f2c84d962be77a0e6e1befab21fe538e7a1
WoodShack <woodhousie@gmail.com>
2025-09-25 10:34:23 -0400

    Update video page

--- components/images.jsx
+++ components/images.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const Images = () => {
     return (
         <div>
-            <h1>My Images</h1>
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Card className="header-card">
+                    My Images
+                </Card>
+            </div>
+
         </div>
     );
 };
--- components/videos.jsx
+++ components/videos.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const videoData = [
+    {
+        title: "React Tutorial",
+        url: "https://www.youtube.com/embed/SqcY0GlETPk"
+    },
+    {
+        title: "Bootstrap Tutorial",
+        url: "https://www.youtube.com/embed/4sosXZsdy-s"
+    }
+];
 
 const Videos = () => {
     return (
         <div>
-            <h1>My Videos</h1>
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Card className="header-card">
+                    My Videos
+                </Card>
+            </div>
+
+            <Row className="justify-content-center">
+                {videoData.map((video, idx) => (
+                    <Col md={11} key={idx} className="mb-4 mx-3">
+                        <Card>
+                            <div className="ratio ratio-16x9">
+                                <iframe
+                                    src={video.url}
+                                    title={video.title}
+                                    allowFullScreen
+                                    style={{ border: 0 }}
+                                />
+                            </div>
+                            <Card.Body>
+                                <Card.Title>{video.title}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 };
